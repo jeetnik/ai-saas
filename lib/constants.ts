b7ac31bebf35995d20626bdb851bbce10a9c684d
saijeetnik <saijeet01@gmail.com>
2025-04-03 18:08:08 +0530

    add the complete

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,8 +3,8 @@ export const plansMap = [
     id: "basic",
     name: "Basic",
     description: "Get started with SpeakEasy!",
-    price: "10",
-    items: ["3 Blog Posts", "3 Transcription"],
+    price: "1",
+    items: ["10 Blog Posts", "10 Transcription"],
     paymentLink: "https://buy.stripe.com/test_aEU9D35X65fH0MMeUW",
     priceId:
       process.env.NODE_ENV === "development"
@@ -28,4 +28,4 @@ export const plansMap = [
 export const ORIGIN_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://speakeasyai-demo.vercel.app";
+    : "https://ai-saas-five-bice.vercel.app/";
